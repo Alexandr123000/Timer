1ff22982dfdbb14d9ee2a2ba5fbf1a7bbe5d1326
Alexandr123000 <alexandrt10000@gmail.com>
2026-02-17 21:05:30 +0300

    WIP: development process

--- Timer.py
+++ Timer.py
@@ -1,1 +1,22 @@
+from tkinter import *
+from tkinter import ttk
 
+def timer(): #timer function
+    global seconds
+    if seconds > 0:
+        label.config(text=str(seconds))
+        seconds -= 1
+        main.after(1000, timer)
+    else:
+        label.config(text="time is up")
+
+main = Tk()
+
+label = Label(main, text="mmm")
+label.grid(row=4, column=7)
+
+seconds = 20
+
+timer()
+
+main.mainloop()
