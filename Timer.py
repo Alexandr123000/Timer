636e67f87b8db3a7de62806d20dba74a96f50857
Alexandr123000 <alexandrt10000@gmail.com>
2026-02-18 13:03:30 +0300

    WIP: development process

--- Timer.py
+++ Timer.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 
-def timer(): # timer working
+def timer(): # timer working function
     global seconds
     if seconds > 0:
         label.config(text=str(seconds))
