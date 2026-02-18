571cd2b92be1baa8465b46bb92e1b664aa4428c8
Alexandr123000 <alexandrt10000@gmail.com>
2026-02-18 18:35:45 +0300

    style: add a style

--- Timer.py
+++ Timer.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from tkinter import ttk
+from turtle import bgcolor
 
+# functions
 def timer(): # function with timer
     global seconds
     if seconds > 0:
@@ -10,7 +12,27 @@ def timer(): # function with timer
     else:
         label.config(text="time is up")
 
+# Main
 main = Tk()
+main.title("Timer")
+main.configure(bg="skyblue")
+
+# Styles
+style = ttk.Style()
+style.configure("TSeparator", bgcolor="red")
+
+#Separators
+ttk.Separator(
+    master=main,
+    orient=VERTICAL,
+    style="TSeparator",
+    class_= ttk.Separator,
+    )
+
+
+
+
+l = Toplevel(main, bg="blue")
 label = Label(main, text="mmm")
 label.grid(row=4, column=7)
 seconds = 20
