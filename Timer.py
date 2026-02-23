763b2dd4b3031666177cc8a5074bd934a5615e52
Alexandr123000 <alexandrt10000@gmail.com>
2026-02-23 16:52:04 +0300

    feat: add three new buttons

--- Timer.py
+++ Timer.py
@@ -18,52 +18,33 @@ neither_hours_nor_minutes = False
 no_minutes = False
 seconds_of_the_time = 0
 timer_is_paused = False
-timer_was_paused = False
-"""global function_running
-function_running = False"""
 current_number_of_seconds_of_the_time = 0
 current_number_of_hours = "00"
 current_number_of_minutes = "00"
 current_number_of_seconds = "00"
 
+
 # functions
 def timer():
-    print("vvv")
-    global seconds_of_the_time, current_number_of_hours, current_number_of_minutes, current_number_of_seconds, timer_is_paused, timer_was_paused
-    print(timer_was_paused)
-    """if timer_is_paused:
-        seconds_of_the_time = current_number_of_seconds_of_the_time
-        print("isisis")
-        timer_is_paused = False
-        timer_was_paused = True
-        pause_the_timer_button.config(text="PAUSE")
-        print(current_number_of_seconds)
-        return
+    global seconds_of_the_time, current_number_of_hours, current_number_of_minutes, current_number_of_seconds, timer_is_paused
+    if start_the_timer_button["relief"] == "sunken" and pause_the_timer_button["relief"] == "raised":
+        start_the_timer_button.config(relief="raised")
     else:
-        if timer_was_paused == True:
-            seconds_entry.config(text=current_number_of_seconds)
-            minutes_entry.config(text=current_number_of_minutes)
-            hours_entry.config(text=current_number_of_hours)
-            timer_was_paused = False"""
+        start_the_timer_button.config(relief="sunken")
     if timer_is_paused:
-        seconds_entry.config(text=current_number_of_seconds)
-        minutes_entry.config(text=current_number_of_minutes)
-        hours_entry.config(text=current_number_of_hours)
-        seconds_of_the_time = 0
-        timer_is_paused = False
-        timer_was_paused = True
-        print(timer_was_paused)
-        print(current_number_of_seconds_of_the_time)
+        seconds_of_the_time = current_number_of_seconds_of_the_time
+        pause_the_timer_button.config(text="PAUSE", relief="raised")
         return
-
     else:
+        if pause_the_timer_button.cget("text") == "PAUSED":
+            pause_the_timer_button.config(text="PAUSE", relief="raised")
         seconds_of_the_time = int(hours.get()) * 3600 + int(minutes.get()) * 60 + int(seconds.get())
         start_number_of_seconds_of_the_time = seconds_of_the_time
     while seconds_of_the_time > -1:
-        if timer_was_paused:
+        if timer_is_paused:
             seconds_of_the_time = current_number_of_seconds_of_the_time
-            timer_was_paused = False
-            print("ww")
+            timer_is_paused = False
+            return
         number_of_minutes, number_of_seconds = (seconds_of_the_time // 60, seconds_of_the_time % 60)
         if number_of_minutes > 60 or number_of_minutes == 60:
             number_of_hours, number_of_minutes = (number_of_minutes // 60, number_of_minutes % 60)
@@ -111,15 +92,19 @@ def timer():
 
 def pause_the_timer():
     global timer_is_paused, current_number_of_seconds, current_number_of_minutes, current_number_of_seconds, timer_is_paused, seconds_of_the_time
+    if start_the_timer_button["relief"] == "sunken":
+        start_the_timer_button.config(relief="raised")
+    if timer_is_paused:
+        timer_is_paused = True
+        return
     timer_is_paused = True
     pause_the_timer_button.config(text="PAUSED")
+    pause_the_timer_button.config(relief="sunken")
     global current_number_of_seconds_of_the_time
     current_number_of_seconds_of_the_time = seconds_of_the_time
-    seconds_of_the_time = 0
     current_number_of_hours = hours_entry.get()
     current_number_of_minutes = minutes_entry.get()
     current_number_of_seconds = seconds_entry.get()
-    print("zzz" + str(current_number_of_seconds_of_the_time))
 
 def reset_the_timer():
     global seconds_of_the_time, current_number_of_seconds_of_the_time, neither_hours_nor_minutes, no_minutes, timer_is_paused
@@ -149,18 +134,29 @@ seconds_entry = Entry(main, textvariable=seconds, width=10, font=15)
 minutes_entry = Entry(main, textvariable=minutes, width=10, font=15)
 hours_entry = Entry(main, textvariable=hours, width=10, font=15)
 
+#Frames
+placing_buttons_frame = Frame(main)
+placing_buttons_frame.grid(row=0, column=2)
+
 #Buttons
-start_the_timer_button = Button(main, text="START", font=20, command=timer)
-pause_the_timer_button = Button(main, text="PAUSE", font=20, command=pause_the_timer)
-reset_the_timer_button = Button(main, text="RESET", font=20, command=reset_the_timer)
+start_the_timer_button = Button(main, text="START", font=50, command=timer)
+pause_the_timer_button = Button(main, text="PAUSE", font=50, command=pause_the_timer)
+reset_the_timer_button = Button(main, text="RESET", font=50, command=reset_the_timer)
+add_a_new_timer_button = Button(placing_buttons_frame, text="Add a timer", font=15)
+delete_a_timer_button = Button(placing_buttons_frame, text="Delete a timer", font=15)
+delete_all_timers_button = Button(placing_buttons_frame, text="Delete all timers", font=15)
 
 #Placing
+placing_buttons_frame.place(relx=0, rely=0)
 seconds_entry.place(relx=0.65, rely=0.35)
 minutes_entry.place(relx=0.55, rely=0.35)
 hours_entry.place(relx=0.45, rely=0.35)
 start_the_timer_button.place(relx=0.6, rely=0.5)
 pause_the_timer_button.place(relx=0.7, rely=0.5)
 reset_the_timer_button.place(relx=0.8, rely=0.5)
+add_a_new_timer_button.pack()
+delete_a_timer_button.pack()
+delete_all_timers_button.pack()
 
 
 main.mainloop()
